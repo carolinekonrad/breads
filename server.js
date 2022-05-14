515e3c6f92a426310a31e5cde40e46e02ac66f1e
Caroline Konrad <ckonrad@live.unc.edu>
2022-05-14 10:16:41 -0400

    sending index and show data

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+
+//configuration
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Welcome to an awesome app about breads!')
+})
+
+//Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+app.listen(PORT, () => {
+    console.log('nomming at port', PORT)
+})
