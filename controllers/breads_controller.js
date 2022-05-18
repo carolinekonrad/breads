2c26d7a9280c1c601a4588f627a784fc18024e04
Caroline Konrad <ckonrad@live.unc.edu>
2022-05-18 17:21:59 -0400

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -17,6 +17,15 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+
+//EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
+})
+
 //SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (bread[req.params.arrayIndex]) {
@@ -49,6 +58,17 @@ breads.post('/', (req, res) => {
 breads.delete('/:indexArray', (req, res) => {
     bread.splice(req.params.indexArray, 1)
     res.status(303).redirect('/breads')
+})
+
+//UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+    if (req.body.gluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 module.exports = breads
